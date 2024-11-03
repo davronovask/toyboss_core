b195ba160786d7fef82ea3f6da939f0c8db0388d
davronovask <adbdulkhaevamuslima07@gmail.com>
2024-11-04 00:35:23 +0600

    добавила запись в settings.py

--- toyboss_core/settings.py
+++ toyboss_core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-p$=-#$(mfjlxvc(mtcf-e^4#m&#&^%suph1i0o+z2$4y8u2-u4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'davronovask2.pythonanywhere.com']
 
 
 # Application definition
